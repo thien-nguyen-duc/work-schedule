0cf52d404c9e3daacf76ffb794f0c46a49db02ab
thien-nguyen-duc <devthiennguyen@gmail.com>
2025-04-22 10:44:14 +0700

    build app

--- src/components/ScheduleCard.vue
+++ src/components/ScheduleCard.vue
@@ -32,7 +32,7 @@
 </template>
 
 <script setup lang="ts">
-    import { ref, computed } from 'vue'
+    import { computed } from 'vue'
     import { Laptop, House, Clock, ChevronDown } from 'lucide-vue-next'
     import { useScheduleStore, DefaultWorkMode } from '../stores/scheduleStore'
     
